a30c5d8c56a22cb548143954087256c1ab0b9b95
Joao-Eduardo-AJ <joaoeduaj@gmai.com>
2024-05-01 16:50:53 -0300

    style: update styles

--- src/components/floating-ball/index.tsx
+++ src/components/floating-ball/index.tsx
@@ -5,7 +5,7 @@ export const FloatingBalls = () => (
     {ballCount.map((ball) => (
       <div
         key={ball}
-        className={`floating-ball-${ball} absolute -z-10 h-64 w-64 bg-primary-300 blur-lg dark:bg-primary-1000 md:h-96 md:w-96`}
+        className={`floating-ball-${ball} absolute -z-10 h-96 w-96 bg-primary-200 dark:bg-primary-900`}
       />
     ))}
   </>
