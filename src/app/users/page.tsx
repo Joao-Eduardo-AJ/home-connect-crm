1fc8168418874694b65cb88a5488a79ecfbc9809
Joao-Eduardo-AJ <joaoeduaj@gmai.com>
2024-06-17 12:38:28 -0300

    feat: adds users page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,30 @@
-const Users = () => {
-  return <p>Hello Users</p>;
-};
+import { TD, TH } from "@/components";
+import { users } from "@/mock";
+
+const Users = () => (
+  <main className="flex flex-col items-center p-4">
+    <h2>Tabela de clientes</h2>
+    <table>
+      <thead>
+        <tr>
+          <TH>Nome</TH>
+          <TH>Contato</TH>
+          <TH variant={{ type: "hiddenSM" }}>Endereço</TH>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={index}>
+            <TD>{user.fullName}</TD>
+            <TD>{user.contact.email}</TD>
+            <TD
+              variant={{ type: "hiddenSM" }}
+            >{`${user.address.state} / ${user.address.city}`}</TD>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </main>
+);
 
 export default Users;
